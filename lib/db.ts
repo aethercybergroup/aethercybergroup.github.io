a67c0d717d2197379aad0828c6954bd444a7e0d2
aethercybergroup <aethercybergroup@gmail.com>
2025-10-30 19:59:06 -0400

    Create db.ts

---
+++ lib/db.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from '@prisma/client';
+
+// Prevent multiple instances of Prisma Client in development
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
+export const prisma: PrismaClient =
+  globalThis.prisma ||
+  new PrismaClient({
+    log: ['query'],
+  });
+
+if (process.env.NODE_ENV !== 'production') {
+  globalThis.prisma = prisma;
+}
