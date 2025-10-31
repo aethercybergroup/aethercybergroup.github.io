acde04f16792f6a6d1663544da0015c1e4bf33df
aethercybergroup <aethercybergroup@gmail.com>
2025-10-30 20:12:05 -0400

    Create partners page

---
+++ app/home/partners/page.tsx
@@ -0,0 +1,14 @@
+export default function PartnersPage() {
+  return (
+    <div className="space-y-6">
+      <h1 className="text-2xl font-bold">Our Partners</h1>
+      <p className="text-gray-300">We collaborate with leading organizations to deliver world-class cybersecurity solutions.</p>
+      <ul className="list-disc list-inside space-y-2">
+        <li>CyberSecure Corp.</li>
+        <li>DataShield Technologies</li>
+        <li>SecureNet Solutions</li>
+        <li>TrustedGuard Innovations</li>
+      </ul>
+    </div>
+  );
+}
